0037becfaee516e165c6b54e284e4bc50db6633a
YehChengLin <113669900+YehChengLin@users.noreply.github.com>
2024-02-26 19:18:48 +0800

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -16,86 +16,55 @@ number of anagrams for each word listed below:
     * spear -> 12 anagrams
 """
 
-import time                   # This file allows you to calculate the speed of your algorithm
+import time
 
 # Constants
-FILE = 'dictionary.txt'       # This is the filename of an English dictionary
-EXIT = '-1'                   # Controls when to stop the loop
-dictionary = {}
-
+FILE = 'dictionary.txt'  # This is the filename of an English dictionary
+EXIT = '-1'  # Controls when to stop the loop
 
 def main():
-    """
-    TODO:
-    """
-    read_dictionary()
+    dictionary = read_dictionary()
     print('Welcome to stanCode "Anagram Generator" (or -1 to quit)')
     while True:
         s = input('Find anagrams for: ')
         if s == EXIT:
             break
         start = time.time()
-        result = find_anagrams(s, '', [])
+        result = find_anagrams(s, dictionary)
         print(result)
         end = time.time()
         print('----------------------------------')
         print(f'The speed of your anagram algorithm: {end-start} seconds.')
-    ####################
-    #                  #
-    #       TODO:      #
-    #                  #
-    ####################
-    
 
 def read_dictionary():
-    global dictionary
+    dictionary = set()
     with open(FILE, 'r') as f:
         for line in f:
-            line2 = line.replace('\n', "")
-            dictionary[line2] = len(line2)
-            
+            dictionary.add(line.strip())
+    return dictionary
 
-def find_anagrams(s, prefix, result):
-    if s == '':
-        result.append(prefix)
-        print('Searching....')
-        print('Found:  ', prefix)
-        return result
-    for i in range(len(s)):
-        if s[0] != s[i] or i == 0:
-            s = swap(s, i)
-            prefix = list(prefix)
-            prefix.append(s[0])
-            prefix = ''.join(prefix)
-            if has_prefix(prefix, len(s)+len(prefix)-1):
-                remain = ''
-                for j in range(1, len(s)):
-                    remain = list(remain)
-                    remain.append(s[j])
-                    remain = ''.join(remain)
-                result = find_anagrams(remain, prefix, result)
-            prefix = list(prefix)
-            prefix.pop()
-            prefix = ''.join(prefix)
-            swap(s, i)
+def find_anagrams(s, dictionary):
+    result = []
+    find_anagrams_helper(s, '', result, dictionary)
     return result
 
+def find_anagrams_helper(s, prefix, result, dictionary):
+    if prefix in dictionary and prefix not in result:
+        print('Searching....')
+        print('Found: ', prefix)
+        result.append(prefix)
+    if not s:
+        return
+    for i in range(len(s)):
+        if i > 0 and s[i] == s[i-1]:
+            continue  # Skip duplicate characters to avoid redundant work
+        find_anagrams_helper(s[:i] + s[i+1:], prefix + s[i], result, dictionary)
 
-def has_prefix(sub_s, length):
-    global dictionary
-    for key in dictionary:
-        if dictionary[key] == length and key.startswith(sub_s):
+def has_prefix(sub_s, dictionary):
+    for word in dictionary:
+        if word.startswith(sub_s):
             return True
     return False
-
-
-def swap(s, i):
-    s = list(s)
-    temp = s[0]
-    s[0] = s[i]
-    s[i] = temp
-    s = ''.join(s)
-    return s
 
 if __name__ == '__main__':
     main()
